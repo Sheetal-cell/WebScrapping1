df6b2f11898003bab4f655a89b332af3a9c2580c
Sheetal-cell <sheetalbajajgodda@gmail.com>
2026-08-13 18:35:58 +0530

    applied BeauttifulSoup

--- Webscrapping1..py
+++ Webscrapping1..py
@@ -1,6 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 web=requests.get("https://portfoliosheetal.vercel.app/")
 print(web)
 
 #output: <Response [200]>
-print(web.content)  #gives html code of that file
+#print(web.content)  #gives html code of that file
+print(web.url)
+print(web.status_code)
+
+soup=BeautifulSoup(web.content,"html.parser")
+print(soup.prettify)
